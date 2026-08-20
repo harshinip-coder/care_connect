786f6b8d851a668c9aa2afdef2b1fa90cf6f6198
harshinip-coder <harshini@gmail.com>
2026-08-20 18:19:50 +0530

    Fix build.sh and WhiteNoise static storage for Render deployment

--- careconnect/settings.py
+++ careconnect/settings.py
@@ -164,7 +164,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
